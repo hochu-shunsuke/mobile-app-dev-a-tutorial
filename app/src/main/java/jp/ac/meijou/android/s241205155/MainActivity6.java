e366d8159febc4d5d61667bb9c6a9dc3c65b1e9d
Shunsuke Hochu <124444636+hochu-shunsuke@users.noreply.github.com>
2025-09-04 14:11:32 +0900

    画像取得実装．パラメタ設定可能に

--- app/src/main/java/jp/ac/meijou/android/s241205155/MainActivity6.java
+++ app/src/main/java/jp/ac/meijou/android/s241205155/MainActivity6.java
@@ -2,6 +2,7 @@ package jp.ac.meijou.android.s241205155;
 
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -12,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 import jp.ac.meijou.android.s241205155.databinding.ActivityMain6Binding;
 import okhttp3.Call;
@@ -38,12 +41,26 @@ public class MainActivity6 extends AppCompatActivity {
         });
 
         binding.Main6button.setOnClickListener(view -> {
-            var text = binding.Main6editTextTextPersonName.getText().toString();
-            var url = Uri.parse("https://placehold.jp/500x500.png")
-                    .buildUpon()
-                    .appendQueryParameter("text", text)
-                    .build()
-                    .toString();
+            String textSize = binding.Main6TextSizeInput.getText().toString();
+            String textColor = binding.Main6TextColorInput.getText().toString();
+            String bgColor = binding.Main6BgColorInput.getText().toString();
+            String widthStr = binding.Main6WidthInput.getText().toString();
+            String heightStr = binding.Main6HeightInput.getText().toString();
+            String text = binding.Main6TextInput.getText().toString();
+            int width = 500;
+            int height = 500;
+            int size = 234;
+            try {
+                width = Integer.parseInt(widthStr);
+                height = Integer.parseInt(heightStr);
+                size = Integer.parseInt(textSize);
+            } catch (NumberFormatException ignored) {}
+            // URLエンコード
+            String encodedText = null;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                encodedText = URLEncoder.encode(text, StandardCharsets.UTF_8);
+            }
+            String url = "https://placehold.jp/" + size + "/" + textColor + "/" + bgColor + "/" + width + "x" + height + ".png?text=" + encodedText;
             getImage(url);
         });
     }
